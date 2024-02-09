ff483f16b376e65527836bb2b930bbfbb5599a42
FernanApps <dev.fernan.apps@gmail.com>
2024-02-09 17:26:01 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    id("com.github.johnrengelman.shadow").version("7.1.2")
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.sparkjava:spark-kotlin:1.0.0-alpha")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+
+val mainClassBase = ""
+val mainClassBotNew = mainClassBase + "MainKt"
+
+val shadow  = "shadow"
+val nameShadowBotNew = shadow + "RestApi"
+
+tasks.register<Jar>("shadowJarRestApi") {
+    dependsOn("shadowJar")
+    manifest {
+        attributes["Main-Class"] = mainClassBotNew
+    }
+    archiveBaseName.set(nameShadowBotNew)
+    archiveClassifier.set("")
+    archiveVersion.set("")
+    println(projectDir)
+    println(tasks.shadowJar.get().archiveFile.get())
+    from(zipTree(tasks.shadowJar.get().archiveFile.get()))
+    destinationDirectory.set(projectDir)
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "api_1_8"
+
